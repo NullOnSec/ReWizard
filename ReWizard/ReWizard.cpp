c04cbf1db71b04c58377d66b323a116cb8b0d97a
Trokolo <you@example.com>
2025-08-05 13:22:45 +0200

    Add project files.

---
+++ ReWizard/ReWizard.cpp
@@ -0,0 +1,12 @@
+﻿// ReWizard.cpp : Defines the entry point for the application.
+//
+
+#include "ReWizard.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
